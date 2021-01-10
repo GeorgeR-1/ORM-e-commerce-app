b4868c5c33c7c4fb15bf8f335e4c575365848b40
GeorgeLondon1 <radac.george@yahoo.com>
2021-01-10 21:24:50 +0200

    real-e-commerce - ORM -> OrderService + OrderRepository

---
+++ src/main/java/com/cybertek/repository/OrderRepository.java
@@ -0,0 +1,13 @@
+package com.cybertek.repository;
+
+import com.cybertek.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Long> {
+
+
+
+
+}
--- src/main/java/com/cybertek/service/OrderService.java
+++ src/main/java/com/cybertek/service/OrderService.java
@@ -1,10 +1,54 @@
 package com.cybertek.service;
 
+import com.cybertek.model.Order;
+import com.cybertek.repository.OrderRepository;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderService {
-    //TODO continue from order service
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Order create(Order order) throws Exception {
+
+        Optional<Order> foundedOrder = orderRepository.findById(order.getId());
+
+        if(foundedOrder.isPresent()){
+            throw new Exception("This order already exists");
+        }
+
+        return orderRepository.save(order);
+    }
+
+    public List<Order> readAll(){
+        return orderRepository.findAll(Sort.by("orderDate").descending());
+    }
+
+    public Order readById(Long id) throws Exception {
+
+        return orderRepository.findById(id)
+                .orElseThrow(() -> new Exception("This order does not exist"));
+    }
+
+    public void update(Order order) throws Exception {
+
+        orderRepository.findById(order.getId())
+                .orElseThrow(() -> new Exception("This order does not exist"));
+
+        orderRepository.save(order);
+    }
+
+
+
 
 
 
